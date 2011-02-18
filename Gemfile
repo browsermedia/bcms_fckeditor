6de1227135828115560cf71654ff000a9cf232d1
peakpg <peakpg@gmail.com>
2011-02-18 17:59:42 -0500

    Module loads similarly to other gems.

--- Gemfile
+++ Gemfile
@@ -8,5 +8,7 @@ group :development, :test do
 	gem 'sqlite3-ruby', :require => 'sqlite3'
 end
 
-
 gem "mysql"
+
+# Load this module as a gem, so that engines and other gem requirements work.
+gem 'bcms_fckeditor', :path=>"."
--- lib/bcms_fckeditor/engine.rb
+++ lib/bcms_fckeditor/engine.rb
@@ -1,8 +1,9 @@
 require 'browsercms'
-
 module BcmsFckeditor
   class Engine < Rails::Engine
     include Cms::Module
+    
+    puts "processing BcmsFckeditor::Engine"    
     
     initializer "extend section_controller" do 
       require 'bcms_fckeditor/section_controller_extensions'
